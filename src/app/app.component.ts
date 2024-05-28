81ed54ee08fe054f5375059d3fdd1e737a942192
Murilo517 <murilosena07@hotmail.com>
2024-05-28 14:48:42 -0300

    feat: router and chatbot component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatbotComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
